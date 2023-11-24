a72d6710637e0a44ad55b4604209273b016e7046
Your Name <you@example.com>
2023-11-23 21:30:14 -0600

    updated columns_order = ['League', 'Hit %', 'Miss %', 'Push %', 'Total Count', 'Prop', 'Prop Hit %', 'Prop Miss %', 'Prop Push %', 'Prop Total Count'] interleaved_df = interleaved_df[columns_order]
    interleaved_df['Prop Hit Count'] = round(interleaved_df['Hit %'] * interleaved_df['Prop Total Count'])
    interleaved_df['Prop Miss Count'] = round(interleaved_df['Miss %'] * interleaved_df['Prop Total Count'])
    interleaved_df['Prop Push Count'] = round(interleaved_df['Push %'] * interleaved_df['Prop Total Count'])
    
    columns_order += ['Prop Hit Count', 'Prop Miss Count', 'Prop Push Count']
    interleaved_df = interleaved_df[columns_order]

--- streak_combined_resulter.py
+++ streak_combined_resulter.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import sheets
 import datetime 
 
 # Set the directory path
@@ -12,11 +11,14 @@ files = [file for file in os.listdir(directory_path) if file.endswith("_results.
 # Initialize an empty dataframe
 merged_df = pd.DataFrame()
 
+
 # Loop through the files and concatenate them into the merged dataframe
 for file in files:
     file_path = os.path.join(directory_path, file)
     df = pd.read_csv(file_path)
     merged_df = pd.concat([merged_df, df], ignore_index=True)
+    
+merged_df.to_csv('out.csv')
 
 # Check if 'Hit' column exists before converting it to boolean
 if 'Hit' in merged_df.columns:
@@ -25,17 +27,32 @@ if 'Hit' in merged_df.columns:
     # Rename 'Hit' values to 'Miss' for better clarity
     merged_df['Hit'] = merged_df['Hit'].replace({True: 'Hit', False: 'Miss'})
 
-    # Calculate the overall percentage of Hit and Miss
-    total_rows = len(merged_df)
-    hit_count = (merged_df['Hit'] == 'Hit').sum()
-    miss_count = (merged_df['Hit'] == 'Miss').sum()
+
+    # Calculate the overall percentage of Hit, Miss, and Push
+    total_rows = float(merged_df['Streak'].count())
+
+    over_hit_count = float(((merged_df['Streak'] == 'Over') & (merged_df['Prop Result'] == 'Over')).sum())
+    under_hit_count = float(((merged_df['Streak'] == 'Under') & (merged_df['Prop Result'] == 'Under')).sum())
+    hit_count = over_hit_count + under_hit_count
+    push_count = float((merged_df['Prop Result'] == 'Push').sum())
+    miss_count = total_rows - (hit_count + push_count)
+    
+    print('hit_count', hit_count, 'miss_count', miss_count, 'push_count', push_count)
+    print((hit_count+miss_count+push_count))
 
     # Check if there are any NaN values in the counts and fill them with 0
-    hit_percent = (hit_count / total_rows  ) if not pd.isna(hit_count) else 0.0
-    miss_percent = (miss_count / total_rows ) if not pd.isna(miss_count) else 0.0
+    hit_percent = (hit_count / total_rows) if not pd.isna(hit_count) else 0.0
+    miss_percent = (miss_count / total_rows) if not pd.isna(miss_count) else 0.0
+    push_percent = (push_count / total_rows) if not pd.isna(push_count) else 0.0
+    
+    print('hit_percent',hit_percent,'miss_percent',miss_percent,'push_percent',push_percent)
+    print((hit_percent+miss_percent+push_percent))
+    
+    merged_df.to_csv('out2.csv')
 
-    # Calculate the percentage of Hit and Miss for each League
-    league_percentages = merged_df.groupby('League')['Hit'].value_counts(normalize=True).unstack() 
+    # Calculate the percentage of Hit, Miss, and Push for each League
+    league_percentages = merged_df.groupby('League')['Hit'].value_counts(normalize=True).unstack(fill_value=0)
+    league_percentages['Push Percentage'] = merged_df.groupby('League')['Prop Result'].value_counts(normalize=True).unstack().get('Push', 0)
     league_percentages = league_percentages.rename(columns={'Hit': 'Hit Percentage', 'Miss': 'Miss Percentage'})
 
     # Add "Total Count" column for overall
@@ -43,8 +60,9 @@ if 'Hit' in merged_df.columns:
     league_percentages = pd.merge(league_percentages, league_totals, on='League', how='left')
     league_percentages = league_percentages.rename(columns={'Hit': 'Total Count'})
 
-    # Calculate the percentage of Hit and Miss for each Prop within each League
-    prop_percentages = merged_df.groupby(['League', 'Prop'])['Hit'].value_counts(normalize=True).unstack() 
+    # Calculate the percentage of Hit, Miss, and Push for each Prop within each League
+    prop_percentages = merged_df.groupby(['League', 'Prop'])['Hit'].value_counts(normalize=True).unstack(fill_value=0)
+    prop_percentages['Prop Push Percentage'] = merged_df.groupby(['League', 'Prop'])['Prop Result'].value_counts(normalize=True).unstack().get('Push', 0)
     prop_percentages = prop_percentages.rename(columns={'Hit': 'Prop Hit Percentage', 'Miss': 'Prop Miss Percentage'})
     prop_percentages = prop_percentages.fillna(0.0)
 
@@ -59,8 +77,9 @@ if 'Hit' in merged_df.columns:
     # Add "Overall" row for overall percentages
     overall_row = pd.DataFrame({
         'League': ['Overall'],
-        'Hit Percentage': [round(hit_percent, 1)],
-        'Miss Percentage': [round(miss_percent, 1)],
+        'Hit Percentage': [round(hit_percent, 2)],
+        'Miss Percentage': [round(miss_percent, 2)],
+        'Push Percentage': [round(push_percent, 2)],
         'Total Count': [total_rows]
     })
 
@@ -68,19 +87,18 @@ if 'Hit' in merged_df.columns:
     interleaved_df = pd.concat([league_percentages.reset_index(), prop_percentages], ignore_index=True).sort_values(
         by=['League', 'Hit Percentage'])
         
-        
     interleaved_df = pd.concat([overall_row, interleaved_df], ignore_index=True)     
 
     # Reorder columns with "League" first
-    columns_order = ['League', 'Hit Percentage', 'Miss Percentage', 'Total Count', 'Prop', 'Prop Hit Percentage', 'Prop Miss Percentage', 'Prop Total Count']
+    columns_order = ['League', 'Hit Percentage', 'Miss Percentage', 'Push Percentage', 'Total Count', 'Prop', 'Prop Hit Percentage', 'Prop Miss Percentage', 'Prop Push Percentage', 'Prop Total Count']
     interleaved_df = interleaved_df[columns_order]
 
     # Save the combined percentages to a CSV file with single-digit precision
-    interleaved_df.to_csv('streak_data/combined_summary_report.csv', index=False, float_format='%.3f')
+    interleaved_df.to_csv('streak_data/combined_summary_report.csv', index=False, float_format='%.2f')
     
     today = datetime.datetime.now()
     formatted_date = today.strftime("%Y-%m-%d %H:%M")      
     
-    sheets.write_to_spreadsheet('streak_data/combined_summary_report.csv',"Last Five Streaker",'Combined',add_column_name="Updated",add_column_data=formatted_date,index=0,overwrite=True,append=False)
+    #sheets.write_to_spreadsheet('streak_data/combined_summary_report.csv',"Last Five Streaker",'Combined',add_column_name="Updated",add_column_data=formatted_date,index=0,overwrite=True,append=False)
 else:
     print("The 'Hit' column does not exist in the dataframe.")
